82788375e79d4132bc50f0f2bfdf874723413522
Kyle Thompson <kyle@kylekthompson.com>
2016-11-05 22:46:44 -0500

    tropo: tropo is working :tada:

--- server/models/Pill.js
+++ server/models/Pill.js
@@ -15,17 +15,17 @@ class Pill extends Model {
     return {
       required: [
         'pillFile',
-        'contacts',
+        'numbers',
       ],
       optional: [],
     };
   }
 
-  static identipill(pillFile, contacts, name) {
-    return super.modelValidations({ pillFile, contacts }, this.identipillAttributes)
+  static identipill(pillFile, numbers, name) {
+    return super.modelValidations({ pillFile, numbers }, this.identipillAttributes)
       .then(pill => Vision.classify(`${pill.pillFile.destination}/${pill.pillFile.filename}`))
       .then(pillName => TextToSpeech.toSpeech(pillName, `Identified! That's ${pillName}.`).then(pillAudio => ({ pillName, pillAudio })))
-      .then(pill => Tropo.sendMessageToNumbers(`${name} has just taken ${pill.pillName}`, JSON.parse(contacts)).then(() => pill));
+      .then(pill => Tropo.sendMessageToNumbers(`${name} has just taken ${pill.pillName}`, JSON.parse(numbers)).then(() => pill));
   }
 }
 
--- server/models/Tropo.js
+++ server/models/Tropo.js
@@ -1,22 +1,13 @@
-/* globals Headers */
 const fetch = require('isomorphic-fetch');
 
 class Tropo {
   static sendMessageToNumbers(message, numbers) {
-    const headers = new Headers({ 'X-Tropo-Message': message });
-
-    numbers.forEach((number) => {
-      const formattedNumber = number.indexOf('+') >= 0
-        ? number
-        : `+1${number}`;
-
-      headers.append('X-Tropo-Number', formattedNumber);
-    });
-
-    return fetch(`https://api.tropo.com/1.0/sessions?action=create&token=${process.env.TROPO_MESSAGE_KEY}`, {
-      method: 'GET',
-      headers,
-    });
+    return Promise.all(
+      numbers.map(number => number.replace(/\D+/, '').replace('+', '')).map(number =>
+        fetch(`https://api.tropo.com/1.0/sessions?action=create&token=${process.env.TROPO_MESSAGE_KEY}&number=${number}&msg=${message.replace(' ', '+')}`, {
+          method: 'GET',
+        }))
+    );
   }
 }
 
